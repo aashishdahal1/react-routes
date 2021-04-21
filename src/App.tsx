77eb56f36b7e649791cc8e18161ae60699b6c7e8
headaashish <aashdahal@gmail.com>
2021-04-21 09:36:37 +0545

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Counter from './pages/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <Switch>
+      
+      {/* <Route exact path="/about" component={About}/> */}
+        <Route exact={true} path="/about">
+          <About />
+        </Route>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/">
+          I am a new route
+        </Route>
+        <Route exact path="/counter" component={Counter}/>
+        <Route path="*">
+          404 not found
+        </Route>
+        
+
+      </Switch>
+   </BrowserRouter>
   );
 }
 
